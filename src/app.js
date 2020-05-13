1709c5b7c47df88ae61dd61550c11978573d8082
Angel <angelfrontend@outlook.com>
2020-05-12 23:59:25 -0500

    test new conn 3

--- src/app.js
+++ src/app.js
@@ -30,25 +30,14 @@ app.use(cors(
     'Access-Control-Allow-Origin: *'
 ));
 
-// app.use(myConnection(mysql, {
-// 	host: 'us-cdbr-east-06.cleardb.net',
-// 	user: 'b7a012a450a854',
-// 	password: 'f2b590f1',
-// 	port: 8889,
-//   database: 'heroku_6c5270acb0de78a',
-//   timeout: 60000
-// }));
-
-const connection = mysql.createConnection({
-  host: 'us-cdbr-east-06.cleardb.net',
+app.use(myConnection(mysql, {
+	host: 'us-cdbr-east-06.cleardb.net',
 	user: 'b7a012a450a854',
 	password: 'f2b590f1',
 	port: 8889,
   database: 'heroku_6c5270acb0de78a',
   timeout: 60000
-});
-
-
+}));
 app.use(express.urlencoded({
 	extended: false
 }));
@@ -61,44 +50,33 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
-app.get('/customers', (req, res) => {
-  const sql = 'SELECT * FROM products';
-
-  connection.query(sql, (error, results) => {
-    if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
-    } else {
-      res.send('Not result');
-    }
+// routes
+app.use('/products', productRoutes);
+app.use('/events', eventRoutes);
+app.use('/games', gameRoutes);
+app.use('/admin', adminRoutes);
+app.use('/reservations', reservationRoutes);
+app.use('/machines', machineRoutes);
+app.use('/orders', orderRoutes);
+app.use('/gamesCategories', gamesCategoriesRoutes);
+app.use('/machinesCategories', juegosCategoriesRoutes);
+app.use('/colors', colorRoutes);
+app.use('/carousel', carouselRoutes);
+app.use("/", (req, res)=>{
+  req.getConnection((err, conn) => {
+    conn.query("SELECT * FROM products", (err, prods) => {
+      if (err) {
+        res.send({
+          error: err,
+          success: false
+        });
+      }else{
+        console.log(prods);
+        res.json(prods);
+      }
+    });
   });
 });
-// routes
-// app.use('/products', productRoutes);
-// app.use('/events', eventRoutes);
-// app.use('/games', gameRoutes);
-// app.use('/admin', adminRoutes);
-// app.use('/reservations', reservationRoutes);
-// app.use('/machines', machineRoutes);
-// app.use('/orders', orderRoutes);
-// app.use('/gamesCategories', gamesCategoriesRoutes);
-// app.use('/machinesCategories', juegosCategoriesRoutes);
-// app.use('/colors', colorRoutes);
-// app.use('/carousel', carouselRoutes);
-// app.use("/", (req, res)=>{
-//   req.getConnection((err, conn) => {
-//     conn.query("SELECT * FROM products", (err, prods) => {
-//       if (err) {
-//         res.send({
-//           error: err,
-//           success: false
-//         });
-//       }
-//       console.log(prods);
-//       res.json(prods);
-//     });
-//   });
-// });
 
 // static files
 // app.use(express.static(path.join(__dirname, 'public')));
