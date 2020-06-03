9e864740165696e0e44e0329db7da414fecd6fe7
Angel <angelfrontend@outlook.com>
2020-06-03 00:58:16 -0500

    Game categories

--- src/controllers/gamesCategoriesController.js
+++ src/controllers/gamesCategoriesController.js
@@ -61,85 +61,121 @@ controller.listDataTable = (req, res) => {
 
 controller.create = (req, res) => {
   const data = req.body;
-  req.getConnection((err, conn) => {
-    const query = conn.query('INSERT INTO gameCategories SET ?', [data], (err, rows) => {
-      if(err){
-        console.log(err);
-        res.status(500).send({
-          success: false,
-          message: "El evento no fue creado",
-          data: rows
+  function create(){
+    return new Promise((resolve, reject)=>{
+      req.getConnection((err, conn) => {
+        const query = conn.query('INSERT INTO gameCategories SET ?', [data], (err, rows) => {
+          if(err){
+            reject(err);
+          }else{
+            resolve(rows);
+          }
         });
-      }else{
-        res.status(200).send({
-          success: true,
-          message: "El evento fue creado",
-          data: rows
-        });
-      }
+      });
     });
+  }
+  create().then(rows=>{
+    res.status(200).send({
+      success: true,
+      message: 'There was not any errors',
+      data: [rows]
+    });
+  }).catch(err=>{
+    res.status(500).send({
+      success: false,
+      message: 'There was an error',
+      error: [err]
+    });
+    throw err;
   });
 };
 
 controller.listOne = (req, res) => {
   const { id } = req.params;
-  req.getConnection((err, conn) => {
-    conn.query("SELECT * FROM gameCategories WHERE id = ?", [id], (err, rows) => {
-    res.json(rows);
-    if(err){
-      res.status(500).send({
-        success: false,
-        message: 'Hubo un error',
-        error: err
+  function listOne(){
+    return new Promise((resolve, reject)=>{
+      req.getConnection((err, conn) => {
+        conn.query("SELECT * FROM gameCategories WHERE id = ?", [id], (err, rows) => {
+          if(err){
+            reject(err);
+          }else{
+            resolve(rows);
+          }
+        });
       });
-    }else{
-      res.json(rows);
-    }
     });
+  }
+  listOne().then(rows=>{
+    res.status(200).json(rows);
+  }).catch(err=>{
+    res.status(500).send({
+      success: false,
+      message: 'There was an error',
+      error: [err]
+    });
+    throw err;
   });
 };
 
 controller.edit = (req, res) => {
   const { id } = req.params;
-  const newCustomer = req.body;
-  req.getConnection((err, conn) => {
-    conn.query('UPDATE gameCategories set ? where id = ?', [newCustomer, id], (err, rows) => {
-      if(err){
-        res.status(500).send({
-          success: false,
-          message: 'Hubo un error',
-          error: err
+  const newData = req.body;
+  function update(){
+    return new Promise((resolve, reject)=>{
+      req.getConnection((err, conn) => {
+        conn.query('UPDATE gameCategories set ? where id = ?', [newData, id], (err, rows) => {
+          if(err){
+            reject(err);
+          }else{
+            resolve(rows);
+          }
         });
-      }else{
-        res.status(500).send({
-          success: true,
-          message: 'The game cat was updated succesfully',
-          data: rows
-        });
-      }
+      });
     });
+  }
+  update().then(rows=>{
+    res.status(200).json(rows);
+  }).catch(err=>{
+    res.status(500).send({
+      success: false,
+      message: 'There was an error',
+      error: [err]
+    });
+    throw err;
   });
 };
 
 controller.delete = (req, res) => {
-    const { id } = req.params;
-    req.getConnection((err, connection) => {
-      connection.query('DELETE FROM gameCategories WHERE id = ?', [id], (err, rows) => {
-        if(err){
-          res.status(500).send({
-            success: false,
-            message: 'Hubo un error',
-            error: err
-          });
-        }else{
-          res.status(500).send({
-            success: true,
-            message: 'The game cat was deleted succesfully',
-            data: rows
-          });
-        }
+  const { id } = req.params;
+  function deleteItem(){
+    return new Promise((resolve, reject)=>{
+      req.getConnection((err, connection) => {
+        connection.query('DELETE FROM gameCategories WHERE id = ?',
+        [id],
+        (err, rows) => {
+          if(err){
+            reject(err);
+          }else{
+            resolve(rows);
+          }
+        });
       });
     });
+  }
+  deleteItem().then(rows=>{
+    res.status(200).send({
+      success: false,
+      message: 'There was not any error',
+      data: [rows]
+    });
+  }).catch(err=>{
+    res.status(500).send({
+      success: false,
+      message: 'There was an error',
+      error: [err]
+    });
+    throw err;
+  });
 };
 
 module.exports = controller;
