abd951bd41ec03605bf200b7b3f5315b6faded07
Angel <angelfrontend@outlook.com>
2020-06-02 22:22:14 -0500

    old login

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -8,74 +8,44 @@ controller.register = async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    function register(){
-        return new Promise((resolve, reject)=>{
-            const hashedPassword = await bcrypt.hash(password, 10, (err, hash)=>{
-                if(!err && name != "" && email != ""){
-                    req.getConnection((err, conn) => {
-                        req.body.password = hash;
-                        const query = conn.query('INSERT INTO users set ?', data, (err, rows) => {
-                          if(err){
-                              reject(err);
-                          }else{
-                              resolve(rows);
-                          }
-                        });
-                    });
-                }else{
-                    reject(err);
-                }
+    const hashedPassword = await bcrypt.hash(password, 10, (err, hash)=>{
+        if(!err && name != "" && email != ""){
+            req.getConnection((err, conn) => {
+                req.body.password = hash;
+                const query = conn.query('INSERT INTO users set ?', data, (err, data) => {
+                  console.log(data);
+                  res.json(data);
+                })
             });
-        });
-    }
-    register().then(rows=>{
-        res.status(200).json(rows);
-    }).catch(err=>{
-        res.status(500).send({
-            message: "Hubo un error al crear el usuario",
-            error: [err]
-        });
-        throw err;
+        }else{
+            res.status(500).send({
+                message: "Hubo un error al crear el usuario"
+            });
+        }
     });
 };
 
 controller.login = (req, res) => {
     let data = req.body;
     const password = req.body.password;
-    function login(){
-        return new Promise((resolve, reject)=>{
-            req.getConnection((err, conn)=>{
-                const query = conn.query('SELECT * FROM users where email = ?', data.email, (err, data)=>{
-                    // console.log(data[0].password);
-                    if(err){
-                        reject(err);
-                    }else{
-                        bcrypt.compare(password, data[0].password, (err, check)=>{
-                            if(check){
-                                console.log("check: ", check);
-                                resolve(data[0].password);
-                            }else{
-                                reject(err);
-                            }
-                        });
-                    }
-                    
-                });
-            });
+    req.getConnection((err, conn)=>{
+        const query = conn.query('SELECT * FROM users where email = ?', data.email, (err, data)=>{
+            console.log(data[0].password);
+            bcrypt.compare(password, data[0].password, (err, check)=>{
+                console.log(check);
+                if(check){
+                    res.status(200).json({
+                        success: true,
+                        token: jwt.createToken(data[0])
+                    });
+                }else{
+                    res.status(403).send({
+                        message: "Password incorrrecto"
+                    })
+                }
+            })
         });
-    }
-    login().then(data=>{
-        console.log("Then data: ", data);
-        res.status(200).json({
-            success: true,
-            token: jwt.createToken(data)
-        });
-    }).catch(err=>{
-        res.status(403).send({
-            message: "Password incorrrecto",
-            error: [err]
-        });
-    });
+    })
 };
 
 module.exports = controller;
